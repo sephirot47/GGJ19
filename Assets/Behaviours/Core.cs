05b62541bb73d0f0c4590840e13340e3fba360ea
sephirot47 <victorantondominguez@gmail.com>
2019-01-26 19:50:46 +0100

    Added FMOD and stuff

--- Assets/Behaviours/Core.cs
+++ Assets/Behaviours/Core.cs
@@ -6,11 +6,28 @@ using TMPro;
 
 public class Core : MonoBehaviour
 {
+    public enum State
+    {
+        INTRO,
+        SHUFFLING,
+        SHOW_CONTROLS,
+        PLAYING
+    };
+
     public TextMeshProUGUI countdownText;
     private TruckTrigger truckTrigger;
     Dictionary<PlayerId, int> scores;
-    private float beginTimeSecs;
+    private float beginPlayTimeSecs;
 
+    private State state  = State.INTRO;
+    private float shuffleTimeBegin;
+    public float maxShuffleTime;
+    float shuffleTimeCounter = 0.0f;
+
+    private float showControlsTimeBegin;
+    public float maxShowControlsTime;
+    
+    public GameObject mum, dad, child;
     public static PlayerId winnerPlayerId = PlayerId.CHILD;
 
     void Start()
@@ -19,33 +36,101 @@ public class Core : MonoBehaviour
         scores[PlayerId.CHILD] = 0;
         scores[PlayerId.DAD] = 0;
         scores[PlayerId.MUM] = 0;
-
-        beginTimeSecs = Time.time;
     }
     
     void Update()
     {
-        float passedTimeSecs = (Time.time - beginTimeSecs);
-        float remainingTimeSecs = (10 - passedTimeSecs);
-
-        int seconds = (int)(remainingTimeSecs) % 60;
-        int minutes = (int)(remainingTimeSecs / 60) % 60;
-        string secondsStr = (seconds >= 10 ? "" : "0") + (seconds.ToString());
-        string minutesStr = (minutes >= 10 ? "" : "0") + (minutes.ToString());
-        countdownText.SetText(minutesStr + ":" + secondsStr);
-
-        if (remainingTimeSecs <= 0)
+        if (state == State.INTRO)
         {
-            int maxScore = 0;
-            foreach (KeyValuePair<PlayerId, int> pair in scores)
+
+        }
+        else if (state == State.PLAYING)
+        {
+            float passedTimeSecs = (Time.time - beginPlayTimeSecs);
+            float remainingTimeSecs = (10 - passedTimeSecs);
+
+            int seconds = (int)(remainingTimeSecs) % 60;
+            int minutes = (int)(remainingTimeSecs / 60) % 60;
+            string secondsStr = (seconds >= 10 ? "" : "0") + (seconds.ToString());
+            string minutesStr = (minutes >= 10 ? "" : "0") + (minutes.ToString());
+            countdownText.SetText(minutesStr + ":" + secondsStr);
+
+            if (remainingTimeSecs <= 0)
             {
-                if (pair.Value > maxScore)
+                int maxScore = 0;
+                foreach (KeyValuePair<PlayerId, int> pair in scores)
                 {
-                    maxScore = pair.Value;
-                    Core.winnerPlayerId = pair.Key;
+                    if (pair.Value > maxScore)
+                    {
+                        maxScore = pair.Value;
+                        Core.winnerPlayerId = pair.Key;
+                    }
+                }
+                SceneManager.LoadScene("Win");
+            }
+        }
+        else if (state == State.SHUFFLING)
+        {
+            float shuffleTime = (Time.time - shuffleTimeBegin);
+            float shuffleTimeNorm = shuffleTime / maxShuffleTime;
+            float shuffleTimeLimitNow = Mathf.Clamp((shuffleTimeNorm) * 1.0f, 0.1f, 1.0f);
+
+            if (shuffleTimeNorm < 1.0f)
+            {
+                shuffleTimeCounter += Time.deltaTime;
+                if (shuffleTimeCounter >= shuffleTimeLimitNow)
+                {
+                    Color[] generatedColors = { Color.black, Color.black, Color.black };
+                    GameObject[] characters = { mum, dad, child };
+                    for (int i = 0; i < characters.Length; ++i)
+                    {
+                        GameObject character = characters[i];
+
+                        int j = 0;
+                        float minDistance;
+                        Color characterColor;
+                        do
+                        {
+                            minDistance = 99999.9f;
+                            characterColor = Random.ColorHSV(0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f);
+                            foreach (Color otherColor in generatedColors)
+                            {
+                                float distance = Vector3.Distance(new Vector3(otherColor.r, otherColor.g, otherColor.b),
+                                    new Vector3(characterColor.r, characterColor.g, characterColor.b));
+                                if (distance < minDistance)
+                                {
+                                    minDistance = distance;
+                                }
+                            }
+                        }
+                        while (++j < 50 && minDistance < 1.0);
+                        generatedColors[i] = characterColor;
+                        Debug.Log(minDistance);
+
+                        foreach (SkinnedMeshRenderer mr in character.GetComponentsInChildren<SkinnedMeshRenderer>())
+                        {
+                            if (mr.gameObject.name == "Bottoms" || mr.gameObject.name == "Hats" ||
+                                mr.gameObject.name == "Shoes" || mr.gameObject.name == "Tops")
+                            {
+                                List<Material> materials = new List<Material>();
+                                mr.GetMaterials(materials);
+                                foreach (Material mat in materials)
+                                {
+                                    mat.SetColor("_Color", characterColor);
+                                }
+                            }
+                        }
+                    }
+                    shuffleTimeCounter = 0.0f;
                 }
             }
-            SceneManager.LoadScene("Win");
+        }
+        else if (state == State.SHOW_CONTROLS)
+        {
+            float showControlsTime = (Time.time - showControlsTimeBegin);
+            if (showControlsTime > maxShowControlsTime)
+            {
+            }
         }
     }
 
@@ -53,5 +138,23 @@ public class Core : MonoBehaviour
     {
         int prevScore = scores[playerId];
         scores[playerId] = (prevScore + amount);
+    }
+
+    public void StartShuffle()
+    {
+        state = State.SHUFFLING;
+        shuffleTimeBegin = Time.time;
+    }
+
+    public void StartShowControls()
+    {
+        state = State.SHOW_CONTROLS;
+        showControlsTimeBegin = Time.time;
+    }
+
+    public void StartPlaying()
+    {
+        state = State.PLAYING;
+        beginPlayTimeSecs = Time.time;
     }
 }
---
+++ Assets/Behaviours/MainCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Core core;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartShuffling()
+    {
+        core.StartShuffle();
+    }
+    public void StartShowControls()
+    {
+        core.StartShowControls();
+    }
+    public void StartPlaying()
+    {
+        core.StartPlaying();
+    }
+
+}
